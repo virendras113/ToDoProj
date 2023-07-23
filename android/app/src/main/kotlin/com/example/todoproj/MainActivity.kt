2eb0b98f557cdd5e8900ffc025adcd2f163f7641
virendras113 <91678077+virendras113@users.noreply.github.com>
2023-07-23 19:43:20 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/todoproj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todoproj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'home_page.dart';
+
+void main() async{
+  await Hive.initFlutter();
+
+  var box = await Hive.openBox('mybox');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+    );
+  }
+}
+
